362544779c664415ba579b080691685d9e10a444
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-09-11 06:09:10 -0400

    pdf: create pdf-compression.sh file

---
+++ pdf-compression.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# find all pdf documents in target dir
+# TARGET_DIR=$1
+# OUTPUT_DIR=$2
+
+shopt -s nullglob
+file_names=("${source_dir}/*.pdf")
+shopt -u nullglob
+echo "${arr[@]}" cheese
