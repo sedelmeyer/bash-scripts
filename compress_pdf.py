84d94792dbe6feee9315a1df92274bc81a1afe6b
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-09-12 12:37:24 -0400

    pdf: add initial list_dirfiles func

--- compress_pdf.py
+++ compress_pdf.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import glob
+import os
 import shlex
 import subprocess
 import sys
@@ -20,3 +22,17 @@ def check_pkg_installed(pkg=external_pkg):
             'Run "sudo apt install {0}" and then attempt '
             "script run again.".format(pkg)
         )
+
+
+def list_dirfiles(dirname, ext_type):
+    """
+    Generates a list of files in a directory that have desired extension
+    types as specified.
+    :param directory: str pathname of target parent directory
+    :param ext_list: list of strings specifying target extension types
+        e.g. ['.csv', '.xls']
+    :return: list of filenames for files with matching extension type
+    """
+    filenames = glob.glob("{}/*{}".format(dirname, ext_type))
+    n_files = len(filenames)
+    return n_files, filenames
