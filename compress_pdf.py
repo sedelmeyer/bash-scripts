638d0abfc038f532d3ff89e747bf6a508e9ca917
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-09-13 11:05:00 -0400

    pdf: refactor initial funcs into PDFCompressor class

--- compress_pdf.py
+++ compress_pdf.py
@@ -58,32 +58,40 @@ import subprocess
 import sys
 
 external_pkg = "ghostscript"
+extension_type = ".pdf"
 
 
-def check_pkg_installed(pkg=external_pkg):
-    """Confirms external dependency is install"""
-    output = subprocess.run(shlex.split("dpkg -s {}".format(pkg)), capture_output=True)
-    pkg_exists = output.returncode == 0
-    if pkg_exists:
-        return pkg_exists
-    else:
-        sys.tracebacklimit = 0
-        raise SystemError(
-            "{0} package is required for PDF compression. "
-            'Run "sudo apt install {0}" and then attempt '
-            "script run again.".format(pkg)
+class PDFCompressor:
+    """PDF compression class object"""
+
+    def __init__(self):
+        self.pkg_exists = self.check_pkg_installed(pkg=external_pkg)
+
+    def check_pkg_installed(self, pkg):
+        """Confirms external dependency is install"""
+        output = subprocess.run(
+            shlex.split("dpkg -s {}".format(pkg)), capture_output=True
         )
+        pkg_exists = output.returncode == 0
+        if pkg_exists:
+            return pkg_exists
+        else:
+            sys.tracebacklimit = 0
+            raise SystemError(
+                "{0} package is required for PDF compression. "
+                'Run "sudo apt install {0}" and then attempt '
+                "script run again.".format(pkg)
+            )
 
-
-def list_dirfiles(dirname, ext_type, recursive=True):
-    """
-    Generates a list of files in a directory that have desired extension
-    types as specified.
-    :param directory: str pathname of target parent directory
-    :param ext_list: list of strings specifying target extension types
-        e.g. ['.csv', '.xls']
-    :return: list of filenames for files with matching extension type
-    """
-    filenames = glob.glob("{}/*{}".format(dirname, ext_type), recursive=recursive)
-    n_files = len(filenames)
-    return n_files, filenames
+    def list_dirfiles(dirname, ext_type, recursive=True):
+        """
+        Generates a list of files in a directory that have desired extension
+        types as specified.
+        :param directory: str pathname of target parent directory
+        :param ext_list: list of strings specifying target extension types
+            e.g. ['.csv', '.xls']
+        :return: list of filenames for files with matching extension type
+        """
+        filenames = glob.glob("{}/*{}".format(dirname, ext_type), recursive=recursive)
+        n_files = len(filenames)
+        return n_files, filenames
