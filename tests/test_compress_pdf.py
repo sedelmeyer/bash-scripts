745d5665c03a49d493aab28b2d463a29239b0ce8
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-09-13 11:05:33 -0400

    pdf(test): refactor tests for class methods

--- tests/test_compress_pdf.py
+++ tests/test_compress_pdf.py
@@ -1,19 +1,22 @@
 import sys
 from unittest import TestCase
+from unittest import mock
 
 sys.path.append("..")
 import compress_pdf  # noqa: E402
 
 
-class TestCheckPkg(TestCase):
-    """Test ``check_pkg_installed`` function"""
+class TestPDFCompress(TestCase):
+    """Test class functionality"""
 
-    def test_pkg_exists(self):
+    @mock.patch("compress_pdf.external_pkg", "nano")
+    def test_init_pkg_exists(self):
         """Ensure installed package returns ``True``"""
-        output = compress_pdf.check_pkg_installed(pkg="nano")
-        self.assertTrue(output)
+        PDFComp = compress_pdf.PDFCompressor()
+        self.assertTrue(PDFComp.pkg_exists)
 
-    def test_pkg_does_not_exist(self):
+    @mock.patch("compress_pdf.external_pkg", "sdcfedsawefvvffk")
+    def test_init_pkg_does_not_exist(self):
         """Ensure uninstalled package raises error"""
-        with self.assertRaises(Exception):
-            compress_pdf.check_pkg_installed(pkg="fhiesfdlkf")
+        with self.assertRaises(SystemError):
+            compress_pdf.PDFCompressor()
